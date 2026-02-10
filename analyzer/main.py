6f2ba7fdb8aaa30730fdf25c8bb710253e671226
ZachariasHultman <zacharias.hultman@gmail.com>
2026-02-10 16:49:42 +0100

    uv usage added

--- analyzer/main.py
+++ analyzer/main.py
@@ -76,7 +76,7 @@ def main():
                 item
                 for item in avanza.get_watchlists()
                 if item.get("name")
-                == "Test"  # "Test"  # "Mina favoritaktier"  # "Berkshire"   # "Mina favoritaktier"  # "Äger"
+                == "Mina favoritaktier"  # "Utdelare"  # "Test"  # "Mina favoritaktier"  # "Berkshire"   # "Mina favoritaktier"  # "Äger"
             ),
             None,
         )["orderbookIds"]
